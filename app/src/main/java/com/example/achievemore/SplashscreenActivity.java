b0f26fe3c9cb885a87095d96619e56106d3588c4
CompEng0001 <rb5356h@greenwich.ac.uk>
2020-05-16 20:55:56 +0100

    Initial Build
    AndroidStudio project created, with splash screen template

---
+++ app/src/main/java/com/example/achievemore/SplashscreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.achievemore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashscreenActivity extends AppCompatActivity {
+    /** Duration of wait **/
+private final int SPLASH_DISPLAY_LENGTH = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashscreenActivity.this, MainActivity.class);
+                SplashscreenActivity.this.startActivity(mainIntent);
+                SplashscreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
