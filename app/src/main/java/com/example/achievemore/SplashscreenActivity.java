ae113c24ce7d75093d5bd1dd4600637f6d6d7ad7
CompEng0001 <rb5356h@greenwich.ac.uk>
2020-05-17 17:00:46 +0100

    Created temp splash screen - needs graphics

--- app/src/main/java/com/example/achievemore/SplashscreenActivity.java
+++ app/src/main/java/com/example/achievemore/SplashscreenActivity.java
@@ -5,15 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
-public class SplashscreenActivity extends AppCompatActivity {
+public class SplashscreenActivity extends AppCompatActivity
+{
     /** Duration of wait **/
-private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+
+    AppInfo appInfo = new AppInfo();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        TextView buildTV = findViewById(R.id.idbuildTV);
+        TextView creatorsTV = findViewById(R.id.idcreatorsTV);
+        TextView versionTV = findViewById(R.id.idVersionTV);
+
+        buildTV.append(String.valueOf(appInfo.getAppBuildType()));
+        creatorsTV.append(String.valueOf(appInfo.getAppCreators()));
+        versionTV.append(String.valueOf(appInfo.getAppVersionName()));
+
+
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable()
