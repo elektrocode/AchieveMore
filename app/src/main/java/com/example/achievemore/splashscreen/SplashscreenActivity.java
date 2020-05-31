b37520567de899ca13b0192d7cf5aab9d5c981c6
CompEng0001 <rb5356h@greenwich.ac.uk>
2020-05-31 10:53:37 +0100

    Generic login page, no functionality yet

---
+++ app/src/main/java/com/example/achievemore/splashscreen/SplashscreenActivity.java
@@ -0,0 +1,53 @@
+package com.example.achievemore.ui.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.example.achievemore.AppInfo;
+import com.example.achievemore.MainActivity;
+import com.example.achievemore.R;
+
+public class SplashscreenActivity extends AppCompatActivity
+{
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+
+    AppInfo appInfo = new AppInfo();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appInfo.setAppOpened(getApplicationContext());
+        TextView buildTV = findViewById(R.id.idbuildTV);
+        TextView creatorsTV = findViewById(R.id.idcreatorsTV);
+        TextView versionTV = findViewById(R.id.idVersionTV);
+        TextView timesOpenedTV = findViewById(R.id.idtimesOpenedTV);
+
+        buildTV.append(String.valueOf(appInfo.getAppBuildType()));
+        creatorsTV.append(String.valueOf(appInfo.getAppCreators()));
+        versionTV.append(String.valueOf(appInfo.getAppVersionName()));
+        timesOpenedTV.append(String.valueOf(" " + appInfo.getAppOpened(getApplicationContext())));
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashscreenActivity.this, MainActivity.class);
+                SplashscreenActivity.this.startActivity(mainIntent);
+                SplashscreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
