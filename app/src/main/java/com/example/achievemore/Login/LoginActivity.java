e2d3138ae359de8040792180b39f7af60507d49a
CompEng0001 <rb5356h@greenwich.ac.uk>
2020-05-31 21:24:25 +0100

    refactored code

--- app/src/main/java/com/example/achievemore/Login/LoginActivity.java
+++ app/src/main/java/com/example/achievemore/Login/LoginActivity.java
@@ -16,74 +16,89 @@ import com.google.android.material.textfield.TextInputEditText;
 public class LoginActivity extends AppCompatActivity {
     AppInfo appInfo = new AppInfo();
     LoginDetials LD = new LoginDetials();
+    TextView versionTV = findViewById(R.id.idVersionTV);
+    TextView buildTV = findViewById(R.id.idbuildTV);
+    final TextInputEditText usernameInputTV = findViewById(R.id.idUsernameTIET);
+    final TextInputEditText passwordInputTV = findViewById(R.id.idPasswordTIET);
+    final TextView usernameErrorFieldTV = findViewById(R.id.idusernameErrorTV);
+    final TextView passwordErrorFieldTV = findViewById(R.id.idpasswordErrorTV);
+    final Button loginBtn = findViewById(R.id.idLoginBtn);
+    final Button regsiterBtn = findViewById(R.id.idRegisterBtn);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_login);
             LD.SetUserCreds(getApplicationContext());
-            TextView versionTV = findViewById(R.id.idVersionTV);
-            TextView buildTV = findViewById(R.id.idbuildTV);
             versionTV.append(String.valueOf(appInfo.getAppVersionName()));
             buildTV.append(String.valueOf(appInfo.getAppBuildType()));
-
-            final TextInputEditText usernameInputTV = findViewById(R.id.idUsernameTIET);
-            final TextInputEditText passwordInputTV = findViewById(R.id.idPasswordTIET);
-            final TextView usernameErrorFieldTV = findViewById(R.id.idusernameErrorTV);
-            final TextView passwordErrorFieldTV = findViewById(R.id.idpasswordErrorTV);
-
-            Button loginBtn = findViewById(R.id.idLoginBtn);
-            Button regsiterBtn = findViewById(R.id.idRegisterBtn);
 
             loginBtn.setOnClickListener(new View.OnClickListener()
             {
                 public void onClick(View v)
                 {
-                    if(usernameInputTV.length() == 0)
-                    {
-                        if (passwordInputTV.length() == 0)
-                        {
-                            passwordErrorFieldTV.setText("Enter a username");
-                            passwordErrorFieldTV.setText("Enter a password");
-                            return;
-                        }
-                        else
-                        {
-                            passwordErrorFieldTV.setText("Enter a username");
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        if(passwordInputTV.length() == 0)
-                        {
-                            passwordInputTV.setText("Enter a password");
-                        }
-                        else
-                        {
-                            boolean usernameFound = LD.CheckUsername(usernameInputTV.getText().toString(), getApplicationContext());
-                            boolean passwordCorrect = LD.CheckPassword((passwordInputTV.getText().toString()), getApplicationContext());
+                    login();
+                }
+            });
 
-                            if(!usernameFound)
-                            {
-                                usernameErrorFieldTV.setText("Username does not exist");
-                            }
-                            else
-                            {
-                                if (!passwordCorrect)
-                                {
-                                    passwordErrorFieldTV.setText("password does not match");
-                                }
-                                else
-                                {
-                                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                                    LoginActivity.this.startActivity(mainIntent);
-                                    LoginActivity.this.finish();
-                                }
-                            }
-                        }
-                    }
+            regsiterBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    register(); 
                 }
             });
     }
 
+    public void login()
+    {
+        if(usernameInputTV.length() == 0)
+        {
+            if (passwordInputTV.length() == 0)
+            {
+                usernameErrorFieldTV.setText("Enter a username");
+                passwordErrorFieldTV.setText("Enter a password");
+                return;
+            }
+            else
+            {
+                passwordErrorFieldTV.setText("Enter a username");
+                return;
+            }
+        }
+        else
+        {
+            if(passwordInputTV.length() == 0)
+            {
+                passwordInputTV.setText("Enter a password");
+            }
+            else
+            {
+                boolean usernameFound = LD.CheckUsername(usernameInputTV.getText().toString(), getApplicationContext());
+                boolean passwordCorrect = LD.CheckPassword((passwordInputTV.getText().toString()), getApplicationContext());
+
+                if(!usernameFound)
+                {
+                    usernameErrorFieldTV.setText("Username does not exist");
+                }
+                else
+                {
+                    if (!passwordCorrect)
+                    {
+                        passwordErrorFieldTV.setText("password does not match");
+                    }
+                    else
+                    {
+                        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        LoginActivity.this.startActivity(mainIntent);
+                        LoginActivity.this.finish();
+                    }
+                }
+            }
+        }
+    }
+
+    public void register()
+    {
+
+    }
 }
